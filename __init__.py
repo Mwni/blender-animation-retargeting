50ac25532285026300cff3ff56cad3d3e8f77ad7
Mwni <manuel@tigmar.de>
2023-08-27 04:15:40 +0200

    bump major version

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
 	'name' : 'Animation Retargeting',
 	'author' : 'Mwni',
 	'description' : 'Retarget animations from one rig to another',
-	'version': (1, 0, 0),
+	'version': (1, 1, 0),
 	'blender' : (2, 80, 0),
 	'location' : '3D View > Tools (Right Side) > Retarget',
 	'category' : 'Animation',
